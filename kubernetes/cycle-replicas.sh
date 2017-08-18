f5251e87c7065564ec6ad44dac56fbc8fadbec23
Isaac (Ike) Arias <ike@apprenda.com>
2017-08-18 02:18:37 -0400

    Document demo and add more blinkt colors

--- kubernetes/cycle-replicas.sh
+++ kubernetes/cycle-replicas.sh
@@ -2,11 +2,11 @@ for (( ; ; ))
 do
    echo "Setting Replicas to 16 [ hit CTRL+C to stop]"
    kubectl scale deployment kdemo-dep --replicas=16
-   sleep 12
+   sleep 10
    echo "Setting Replicas to 32 [ hit CTRL+C to stop]"
    kubectl scale deployment kdemo-dep --replicas=32
-   sleep 12
+   sleep 10
    echo "Setting Replicas to 1 [ hit CTRL+C to stop]"
    kubectl scale deployment kdemo-dep --replicas=1
-   sleep 12
+   sleep 10
 done
